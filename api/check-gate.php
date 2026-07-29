f7c4bd517e5a765d5e162583cd0a5ac93e32c3cf
Noo <noo_@bk.ru>
2026-07-30 01:15:28 +0300

    chore: remove temporary diagnostic files

--- api/check-gate.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo 'HTTP_COOKIE: ' . ($_SERVER['HTTP_COOKIE'] ?? 'NOT SET') . "\n";
-echo 'raw site_access: ' . ($_COOKIE['site_access'] ?? 'NOT SET') . "\n";
-if (isset($_COOKIE['site_access'])) {
-    echo 'len: ' . strlen($_COOKIE['site_access']) . "\n";
-    echo 'last10: ' . substr($_COOKIE['site_access'], -10) . "\n";
-}
